6439792ea2e98f53f8812a347e2fd4efc6368bb1
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-21 17:20:14 +0100

    add function to hide numbers + check equals

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,32 +4,45 @@ Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
 
+// seleziono l'elemento con classe 'container' nel DOM
+const numbersEl = document.querySelector('.numbers')
 
 generateNumbers();
 
-
-
+const numsTimeout = setTimeout(hideNums, 30000)
 
 
 
 /* FUNCTIONS */
-function getRandomInteger(min, max) {
-   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 // genera 5 numeri casuali
 function generateNumbers() {
+
    //creo un array che contiene i numeri randomici
    const randomNumbers = [];
 
-   // seleziono l'elemento con classe 'container' nel DOM
-   const containerEl = document.querySelector('.container')
-   
    //eseguo un ciclo for per riempire l'array e creare 5 elementi che contengono i numeri generati
-   for (let i = 0; i < 5; i ++){
-      randomNumbers.push(getRandomInteger(10, 1));
+   for (let i = 0; i < 5; i++) {
+
+      let randomNum = getRandomInteger(100, 1);
+
+      while (randomNumbers.includes(randomNum)){
+         randomNum = getRandomInteger(100, 1);
+      }
+      randomNumbers.push(randomNum);
+      
       const strongEl = `<strong>${randomNumbers[i]}</strong>`;
-      containerEl.insertAdjacentHTML('beforeend', strongEl )
+      numbersEl.insertAdjacentHTML('beforeend', strongEl);
+
    }
    console.log(randomNumbers);
+}
+
+// funzione che nasconde i numeri
+function hideNums() {
+   numbersEl.classList.add('d_none')
+}
+
+
+function getRandomInteger(min, max) {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
